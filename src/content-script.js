98ca6c3e1359dccdf555b1f6a5ab92ae8da2903d
Andrew Talbot <andrew.talbot@elationhealth.com>
2022-09-21 16:13:06 -0700

    initial commit

---
+++ src/content-script.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener(function(msg, sender){
+    if ("hello" == msg) {
+        toggle();
+    }
+    console.log(msg);
+    console.log(sender);
+
+    return {};
+});
+
+var iframe = document.createElement('iframe'); 
+iframe.style.background = "green";
+iframe.style.height = "100%";
+iframe.style.width = "400px";
+iframe.style.position = "fixed";
+iframe.style.top = "0px";
+iframe.style.right = "0px";
+iframe.style.zIndex = "9000000000000000000";
+iframe.frameBorder = "none"; 
+iframe.src = chrome.runtime.getURL("src/popup.html")
+
+document.body.appendChild(iframe);
+
+function toggle(){
+    if(iframe.style.width == "0px"){
+        iframe.style.width="400px";
+    }
+    else{
+        iframe.style.width="0px";
+    }
+}
