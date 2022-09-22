47b654cc133aa4009df9b4d4b00f39ca6b5b3077
Andrew Talbot <andrew.talbot@elationhealth.com>
2022-09-21 17:21:21 -0700

    Run prettier and add pre commit hook

--- src/content-script.js
+++ src/content-script.js
@@ -1,4 +1,4 @@
-var iframe = document.createElement('iframe'); 
+var iframe = document.createElement("iframe");
 
 iframe.style.height = "100%";
 iframe.style.width = "400px";
@@ -6,7 +6,7 @@ iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000";
-iframe.frameBorder = "none"; 
-iframe.src = chrome.runtime.getURL("src/slide-in-panel/index.html")
+iframe.frameBorder = "none";
+iframe.src = chrome.runtime.getURL("src/slide-in-panel/index.html");
 
 document.body.appendChild(iframe);
--- src/slide-in-panel/README.md
+++ src/slide-in-panel/README.md
@@ -1,10 +1,9 @@
-Slide In Panel
-=========
+# Slide In Panel
 
 A CSS3 and JavaScript powered slide-in panel, to quickly show side content, notifications or profile information.
 
 [Article on CodyHouse](https://codyhouse.co/gem/css-slide-in-panel/)
 
 [Demo](https://codyhouse.co/demo/slide-in-panel/)
- 
+
 [Terms](https://codyhouse.co/terms/)
