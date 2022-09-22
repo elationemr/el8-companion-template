79619eb0f4fc68124fe9bb3907d76a94d1e96579
Andrew Talbot <andrew.talbot@elationhealth.com>
2022-09-21 17:15:29 -0700

    Remove lister stuff that's unused

--- src/content-script.js
+++ src/content-script.js
@@ -1,31 +1,12 @@
-chrome.runtime.onMessage.addListener(function(msg, sender){
-    if ("hello" == msg) {
-        toggle();
-    }
-    console.log(msg);
-    console.log(sender);
-
-    return {};
-});
-
 var iframe = document.createElement('iframe'); 
-iframe.style.background = "green";
+
 iframe.style.height = "100%";
 iframe.style.width = "400px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
+iframe.style.zIndex = "9000";
 iframe.frameBorder = "none"; 
 iframe.src = chrome.runtime.getURL("src/slide-in-panel/index.html")
 
 document.body.appendChild(iframe);
-
-function toggle(){
-    if(iframe.style.width == "0px"){
-        iframe.style.width="400px";
-    }
-    else{
-        iframe.style.width="0px";
-    }
-}
