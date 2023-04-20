ddbbcb6461f6f5ce4a54721ecec3358a5bce761f
Andrew Talbot <andrew@talboito.com>
2023-04-20 10:07:45 -0700

    Initial commit of companion starter kit

---
+++ src/content-script.js
@@ -0,0 +1,12 @@
+var iframe = document.createElement("iframe");
+
+iframe.style.height = "100%";
+iframe.style.width = "400px";
+iframe.style.position = "fixed";
+iframe.style.top = "0px";
+iframe.style.right = "0px";
+iframe.style.zIndex = "9000";
+iframe.frameBorder = "none";
+iframe.src = chrome.runtime.getURL("src/slide-in-panel/index.html");
+
+document.body.appendChild(iframe);
